eaadf05ffe6139c32816d546888bb8977c37bd63
gopesh <gopesh185@gmail.com>
2021-08-09 12:20:40 +0530

    Token authentication validation

--- src/main/java/com/inkreta/receivables/ReceivablesApplication.java
+++ src/main/java/com/inkreta/receivables/ReceivablesApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.tecnics.inkreta.config.JwtAuthenticationEntryPoint;
+import com.tecnics.inkreta.config.JwtRequestFilter;
+import com.tecnics.inkreta.config.JwtTokenUtil;
 
 @SpringBootApplication
 public class ReceivablesApplication extends SpringBootServletInitializer{
@@ -16,5 +21,23 @@ public class ReceivablesApplication extends SpringBootServletInitializer{
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ReceivablesApplication.class);
     }
+	
+	@Bean
+	public JwtRequestFilter setJwtRequestFilter()
+	{
+		return new JwtRequestFilter();
+	}
+	
+	@Bean
+	public JwtAuthenticationEntryPoint setJwtAuthenticationEntryPoint()
+	{
+		return new JwtAuthenticationEntryPoint();
+	}
+	
+	@Bean
+	public JwtTokenUtil setJwtTokenUtil()
+	{
+		return new JwtTokenUtil();
+	}
 
 }
